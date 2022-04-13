5127d13ec80713e75cfbea9fa9af84661ff7584c
abhisheka480 <abhisheka480@gmail.com>
2022-04-13 18:24:56 +0530

    added new layers and script to upload to ipfs

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # This file MUST be configured in order for the code to run properly
 
-# Make sure you put all your input images into an 'assets' folder. 
+# Make sure you put all your input images into an 'assets' folder.
 # Each layer (or category) of images must be put in a folder of its own.
 
 # CONFIG is an array of objects where each object represents a layer
@@ -13,12 +13,12 @@
 # 4. required: If the particular layer is required (True) or optional (False). The first layer must always be set to true.
 # 5. rarity_weights: Denotes the rarity distribution of traits. It can take on three types of values.
 #       - None: This makes all the traits defined in the layer equally rare (or common)
-#       - "random": Assigns rarity weights at random. 
-#       - array: An array of numbers where each number represents a weight. 
+#       - "random": Assigns rarity weights at random.
+#       - array: An array of numbers where each number represents a weight.
 #                If required is True, this array must be equal to the number of images in the layer directory. The first number is  the weight of the first image (in alphabetical order) and so on...
 #                If required is False, this array must be equal to one plus the number of images in the layer directory. The first number is the weight of having no image at all for this layer. The second number is the weight of the first image and so on...
 
-# Be sure to check out the tutorial in the README for more details.                
+# Be sure to check out the tutorial in the README for more details.
 
 CONFIG = [
     {
@@ -26,55 +26,55 @@ CONFIG = [
         'name': 'background',
         'directory': 'Background',
         'required': True,
-        'rarity_weights': None,
+        'rarity_weights': "random",
     },
     {
         'id': 2,
         'name': 'body',
         'directory': 'Body',
         'required': True,
-        'rarity_weights': None,
+        'rarity_weights': "random",
     },
     {
         'id': 3,
         'name': 'eyes',
         'directory': 'Expressions',
         'required': True,
-        'rarity_weights': None,
+        'rarity_weights': "random",
     },
     {
         'id': 4,
         'name': 'head_gear',
         'directory': 'Head Gear',
         'required': False,
-        'rarity_weights': None,
+        'rarity_weights': "random",
     },
     {
         'id': 5,
         'name': 'clothes',
         'directory': 'Shirt',
         'required': False,
-        'rarity_weights': None,
+        'rarity_weights': "random",
     },
     {
         'id': 6,
         'name': 'held_item',
         'directory': 'Misc',
-        'required': True,
-        'rarity_weights': None,
+        'required': False,
+        'rarity_weights': "random",
     },
     {
         'id': 7,
         'name': 'hands',
         'directory': 'Hands',
         'required': True,
-        'rarity_weights': None,
+        'rarity_weights': "random",
     },
     {
         'id': 8,
         'name': 'wristband',
         'directory': 'Wristband',
         'required': False,
-        'rarity_weights': None,
+        'rarity_weights': "random",
     },
 ]
